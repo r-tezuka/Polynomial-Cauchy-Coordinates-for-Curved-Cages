d095e7e6ab5b59ea2113738994bf7106143fed72
r-tezuka <ryuta.tezuka2@gmail.com>
2025-01-08 15:00:16 +0900

    update for build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// optional: 以下の設定を必要に応じて変更
-			pages: 'build', // ビルドされた静的ファイルを出力するフォルダー
-			assets: 'build', // 静的アセットの出力先
-			fallback: null,  // `404.html`が必要な場合は指定
-		}),
+		adapter: adapter(),
 		paths: {
-			// GitHub Pagesのサブディレクトリ名を指定
 			base: process.env.NODE_ENV === 'production' ? '/Polynomial-Cauchy-Coordinates-for-Curved-Cages' : '',
 		}
 	}
